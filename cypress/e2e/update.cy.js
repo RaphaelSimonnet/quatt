eedfd30528c4b6acf348c160c9d9ea645d160f07
RaphaelSimonnet <simonnet.raphael@gmail.com>
2024-08-02 16:31:52 +0200

    setup cypess and implement POST / GET

---
+++ cypress/e2e/update.cy.js
@@ -0,0 +1,49 @@
+// Generation of a random emil to be used later
+let generateEmail = () => {
+    const randomString = Math.random().toString(36).substring(2,10)
+    const email = randomString+"@quatt.nl"
+    return email
+}
+
+it ('POST', () => {
+    const xemail = generateEmail()
+    cy.fixture('userPOST').then((payload) => {
+        payload.email = xemail
+        cy.log("EMAIL"+xemail)
+        cy.request({
+            method: "POST",
+            url:"./public/v2/users",
+            headers :
+                {Authorization: 'Bearer 0569f0d940174bc34a169081b7bb122b537b72d2e2dc0c35fc2a249bcf59d137',
+    
+                },
+            body: payload
+    
+        }).then((res)=>{
+            expect(res.status).to.eq(201);
+            expect(res.body).to.have.property("name", "Raphael Simonnet");
+            expect(res.body).to.have.property("gender", "male");
+            expect(res.body).to.have.property("status", "active");
+            expect(res.body.id).to.not.be.null;
+
+            let id = res.body.id
+
+            cy.request({
+                method: "GET",
+                url: './public/v2/users/'+id,
+                headers:{
+                    Authorization: 'Bearer 0569f0d940174bc34a169081b7bb122b537b72d2e2dc0c35fc2a249bcf59d137',
+                }
+            }).then((res) => {
+                expect(res.status).to.eq(200);
+                expect(res.body).to.have.property("name", "Raphael Simonnet");
+                expect(res.body).to.have.property("gender", "male");
+                expect(res.body).to.have.property("status", "active");
+                expect(res.body.id).to.not.be.null;
+
+            })
+
+
+        })
+    })
+})
